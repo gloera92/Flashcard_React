7ccb73b7377974b190ef400b741b30adbb734be8
George Loera <gloera8325@gmail.com>
2021-05-05 12:04:58 -0500

    added a componentmount function, added a getflashcards function

--- src/Components/app.js
+++ src/Components/app.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, {Component} from 'react';
 import Flashcard from './Flashcard/flashcard';
 
@@ -15,6 +16,14 @@ class App extends Component {
         console.log("component mounted");
     }
 
+    async getAllFlashcards(){
+        let response = await axios.get('http://127.0.0.1:8000/flashcard_app');
+        this.setState({
+            flashcards: response.data
+        })
+        console.log("get flashcards")
+    }
+
 
 
 
@@ -23,7 +32,7 @@ class App extends Component {
     render(){
         return(
             <div>
-             <Flashcard />
+             <Flashcard flashcard={this.getAllFlashcards.bind(this)}/>
 
 
             </div>
